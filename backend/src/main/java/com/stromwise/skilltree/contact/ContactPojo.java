4bc74a80db6d8feb40c4a3ab1acf6976aa090ef8
michal <urbaniak.michal@yahoo.com>
2020-08-26 14:49:26 +0200

    change validator hibernate version, change @notempty to @notnull above int phone field inside contactpojo class

--- backend/src/main/java/com/stromwise/skilltree/contact/ContactPojo.java
+++ backend/src/main/java/com/stromwise/skilltree/contact/ContactPojo.java
@@ -1,23 +1,24 @@
 package com.stromwise.skilltree.contact;
 
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
+@AllArgsConstructor
 @Builder
 @Value
 public class ContactPojo {
 
     @NotEmpty(message = "Name may not be empty.")
-    private String name;
+    String name;
     @NotEmpty(message = "Surname may not be empty.")
-    private String surname;
+    String surname;
     @Email(message = "Email should be valid.")
-    private String email;
-    @NotEmpty(message = "Phone may not be empty.")
-    private int phone;
+    String email;
+    @NotNull(message = "Phone may not be empty.")
+    int phone;
     @NotEmpty(message = "Message may not be empty.")
-    private String message;
+    String message;
 }
