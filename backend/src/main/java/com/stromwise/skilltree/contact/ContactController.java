4e068371fb55a98e95b38662eac2a8646e23c93d
michal <urbaniak.michal@yahoo.com>
2020-10-06 19:58:15 +0200

    add @RequiredArgsConstructor to contactcontroller class

--- backend/src/main/java/com/stromwise/skilltree/contact/ContactController.java
+++ backend/src/main/java/com/stromwise/skilltree/contact/ContactController.java
@@ -1,17 +1,15 @@
 package com.stromwise.skilltree.contact;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RequestMapping("/api/skill-tree/v1/contact")
-@RestController
 @RequiredArgsConstructor
+@RestController
 public class ContactController {
 
     private final ContactService contactService;
