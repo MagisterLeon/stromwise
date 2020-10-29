cbb6a9ce8513cc8071db3240f7a6990f6bc89fba
Lukasz Kujawski <lukasz.kujawski@sonalake.com>
2020-10-29 22:21:10 +0100

    implement question carousel

--- frontend/src/app/questions/questions-presenter/questions-presenter.component.ts
+++ frontend/src/app/questions/questions-presenter/questions-presenter.component.ts
@@ -1,30 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {GetQuestionsService} from "../get-questions.service";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {CarouselComponent} from '../../components/carousel/carousel.component';
+import {GetQuestionsService} from '../get-questions.service';
+import {ToolbarApiService} from '../../app-toolbar/toolbar-api.service';
+import {Observable} from 'rxjs';
+import {Question} from '../question';
+
 
 @Component({
   selector: 'st-questions-presenter',
   template: `
-    <mdb-carousel [animation]="'slide'">
-      <mdb-carousel-item>
-        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" alt="First slide">
-      </mdb-carousel-item>
-      <mdb-carousel-item>
-        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg" alt="Second slide">
-      </mdb-carousel-item>
-      <mdb-carousel-item>
-        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Third slide">
-      </mdb-carousel-item>
-    </mdb-carousel>
+    <ng-container *ngIf="questions$ | async as questions">
+      <div class="question-header">
+        <button mat-icon-button (click)="carousel.prev()">
+          <mat-icon>keyboard_arrow_left</mat-icon>
+        </button>
+        <span class="mat-body-strong">{{carousel.getCurrentSlide() + 1}} / {{questions.length}}</span>
+        <button mat-icon-button (click)="carousel.next()">
+          <mat-icon>keyboard_arrow_right</mat-icon>
+        </button>
+      </div>
+      <carousel #carousel>
+        <ng-container *ngFor="let question of questions;">
+          <ng-container *carouselItem>
+            <st-questions-preview [questionModel]="question"></st-questions-preview>
+          </ng-container>
+        </ng-container>
+      </carousel>
+    </ng-container>
   `,
   styleUrls: ['./questions-presenter.component.scss']
 })
 export class QuestionsPresenterComponent implements OnInit {
+  @ViewChild('carousel') carousel: CarouselComponent;
 
-  constructor(private getQuestionsService: GetQuestionsService) {
+  questions$: Observable<Question[]> = this.getQuestionsService.get('Programming');
 
+  constructor(private getQuestionsService: GetQuestionsService,
+              private toolbarApiService: ToolbarApiService) {
   }
 
   ngOnInit(): void {
+    this.toolbarApiService.setIsVisible(true);
+    this.toolbarApiService.setCategory('Programming');
   }
-
 }
+
--- frontend/src/app/questions/questions-preview/questions-preview.component.ts
+++ frontend/src/app/questions/questions-preview/questions-preview.component.ts
@@ -1,41 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {ToolbarApiService} from '../../app-toolbar/toolbar-api.service';
-import {GetQuestionsService} from '../get-questions.service';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {Question} from '../question';
 
 @Component({
   selector: 'st-questions-preview',
   template: `
-    <div class="question-header">
-      <button mat-icon-button>
-        <mat-icon>keyboard_arrow_left</mat-icon>
-      </button>
-      <span class="mat-body-strong">1 / 10</span>
-      <button mat-icon-button>
-        <mat-icon>keyboard_arrow_right</mat-icon>
-      </button>
+    <div class="question">
+       <span class="mat-body" matBadge
+             matBadgeColor="accent"
+             matBadgeOverlap="false"
+             matBadgeIcon="help">{{questionModel.question}}</span>
     </div>
-    <span class="mat-body" matBadge
-          matBadgeColor="accent"
-          matBadgeOverlap="false"
-          matBadgeIcon="help">Dokąd nocą tupta jeż?</span>
-    <mat-button-toggle-group class="" name="answer" vertical>
+    <mat-button-toggle-group name="answer" vertical>
       <mat-button-toggle class="know mat-body-2" value="know">Know</mat-button-toggle>
       <mat-button-toggle class="not-sure mat-body-2" value="notSure">Not sure</mat-button-toggle>
       <mat-button-toggle class="not-know mat-body-2" value="notKnow">Don't know</mat-button-toggle>
     </mat-button-toggle-group>
   `,
-  styleUrls: ['./questions-preview.component.scss']
+  styleUrls: ['./questions-preview.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class QuestionsPreviewComponent implements OnInit {
+export class QuestionsPreviewComponent {
 
-  constructor(private getQuestionsService: GetQuestionsService,
-              private toolbarApiService: ToolbarApiService) {
-  }
-
-  ngOnInit(): void {
-    this.toolbarApiService.setIsVisible(true);
-    this.toolbarApiService.setCategory('Programming');
-
-
-  }
+  @Input() questionModel: Question;
 }
